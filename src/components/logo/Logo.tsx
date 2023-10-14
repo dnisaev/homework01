7467aad294285121b9925d74f53da45a886bd1e1
dnisaev <dnisaev@yandex.ru>
2023-10-14 21:21:46 +0300

    add components

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+        <StyledLogo>
+            <Icon iconId={"code"}/>
+        </StyledLogo>
+    );
+};
+
+const StyledLogo = styled.a`
+
+
+
+`
