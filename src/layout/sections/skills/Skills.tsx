7c96a4855081de733c76c3e03e455623b141e612
dnisaev <dnisaev@yandex.ru>
2023-10-15 00:13:13 +0300

    add SectionTitle.tsx & Skill.tsx $ Skills.tsx

---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Icon} from "../../../components/icon/Icon";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Skill} from "./skill/Skill";
+
+export const Skills = () => {
+    return (
+        <StyledSkills>
+            <SectionTitle>My Skills</SectionTitle>
+            <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+                <Skill iconId={"code"}
+                       title={"html5"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}
+                />
+                <Skill iconId={"css"}
+                       title={"css3"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}
+                />
+                <Skill iconId={"react"}
+                       title={"react"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}
+                />
+                <Skill iconId={"typescript"}
+                       title={"typescript"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}
+                />
+                <Skill iconId={"styled"}
+                       title={"styled components"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}
+                />
+                <Skill iconId={"figma"}
+                       title={"web design"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}
+                />
+            </FlexWrapper>
+        </StyledSkills>
+    );
+};
+
+const StyledSkills = styled.section`
+  background-color: lightgoldenrodyellow;
+  min-height: 100vh;
+`
