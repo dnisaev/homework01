33bd89e590788bfc10492d08eac8d325966e80cf
dnisaev <dnisaev@yandex.ru>
2023-10-15 11:36:30 +0300

    update App.tsx & Header.tsx & Menu.tsx

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,17 +3,18 @@ import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
 
+const headerItems = ["Home", "Skills", "Works", "Testimony", "Contact"]
+
 export const Header = () => {
     return (
         <StyledHeader>
             <Logo/>
-            <Menu/>
+            <Menu menuItems={headerItems}/>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-
   background-color: white;
   display: flex;
   justify-content: space-between;
