9d18174fb9b1f8306d07a2093f9aca433472e666
dnisaev <dnisaev@yandex.ru>
2023-10-15 14:26:40 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Skills} from "./layout/sections/skills/Skills";
 import {Works} from "./layout/sections/works/Works";
 import {Testimony} from "./layout/sections/testimony/Testimony";
 import {Contact} from "./layout/sections/contact/Contact";
+import {Slogan} from "./layout/sections/slogan/Slogan";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
             <Works/>
             <Testimony/>
             <Contact/>
+            <Slogan/>
         </div>
     );
 }
