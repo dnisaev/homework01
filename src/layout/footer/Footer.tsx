c2daf1d1a234474710b2b132cebb9feea68c19c0
dnisaev <dnisaev@yandex.ru>
2023-10-15 14:50:30 +0300

    add Footer.tsx

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../../components/icon/Icon";
+import {FlexWrapper} from "../../components/FlexWrapper";
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <Name>Dmitriy</Name>
+                <SocialList>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"instagram"}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"telegram"}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"vk"}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"linkedin"}/>
+                        </SocialLink>
+                    </SocialItem>
+                </SocialList>
+                <Copyright>© 2023 Dmitriy Isaev, All Rights Reserved.</Copyright>
+            </FlexWrapper>
+        </StyledFooter>
+    );
+};
+
+const StyledFooter = styled.footer`
+  min-height: 20vh;
+  background-color: lightgoldenrodyellow;
+`
+
+const Name = styled.span`
+
+`
+
+const SocialList = styled.ul`
+  display: flex;
+  gap: 30px;
+`
+
+const SocialItem = styled.li`
+
+`
+
+const SocialLink = styled.a`
+
+`
+
+const Copyright = styled.small`
+
+`
