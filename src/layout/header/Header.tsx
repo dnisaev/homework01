ccc8b98892ba4e78670a668ab5b8b9e31c44cae6
dnisaev <dnisaev@yandex.ru>
2023-10-18 00:15:14 +0300

    update Header.tsx

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
-import {Menu} from "../../components/menu/Menu";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
 const headerItems = ["Home", "Skills", "Works", "Testimony", "Contact"]
 
@@ -13,7 +13,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
-                    <Menu menuItems={headerItems}/>
+                    <HeaderMenu menuItems={headerItems}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
@@ -21,5 +21,11 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-  background-color: white;
+  background-color: rgba(31, 31, 32, 0.9);
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99999;
 `
