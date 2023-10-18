d45c175f17186d5da5bdc80bb43cde66ff788826
dnisaev <dnisaev@yandex.ru>
2023-10-18 23:03:43 +0300

    add styles to Main.tsx

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,43 +2,87 @@ import React from 'react';
 import photo from '../../../assets/images/photo.jpg';
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
+import {Theme} from "../../../styles/Theme";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Dmitriy Isaev</Name>
-                    <MainTitle>A Front-end Developer</MainTitle>
-                </div>
-                <Photo src={photo} alt={""}/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-between"}>
+                    <div>
+                        <Greeting>Hi There</Greeting>
+                        <Name>I am <span>Dmitriy Isaev</span></Name>
+                        <MainTitle>A Front-end Developer.</MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt={""}/>
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
 
 const StyledMain = styled.section`
-
+  display: flex;
   min-height: 100vh;
   background-color: lightcyan;
 `
 
-const Photo = styled.img`
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+  
+  &::before {
+    content: "";
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${Theme.colors.accent};
+    z-index: -1;
+    
+    position: absolute;
+    top: -24px;
+    left: 24px;
+  }
+`
 
+const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
-
 `
 
 const MainTitle = styled.h1`
-
-
+  font-size: 27px;
+  font-weight: 400;
 `
 
 const Name = styled.h2`
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 50px;
+  font-weight: 700;
+  letter-spacing: 2.5px;
+  margin: 10px 0;
+  
+  span {
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+      content: "";
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${Theme.colors.accent};
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
+`
 
-
-
+const Greeting = styled.span`
+  font-size: 14px;
+  font-weight: 400;
 `
