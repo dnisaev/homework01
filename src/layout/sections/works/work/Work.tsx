0b6cc798e5d50ef541da668d07558ede056fb21c
dnisaev <dnisaev@yandex.ru>
2023-10-27 00:03:18 +0300

    add adaptive to Works.tsx

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -29,8 +29,8 @@ export const Work = (props: WorkPropsType) => {
 
 const StyledWork = styled.div`
   background-color: ${portfolioTheme.colors.secondaryBg};
-  max-width: 540px;
-  width: 100%;
+  width: 330px;
+  flex-grow: 1;
 
   ${Link} {
     padding: 10px 0;
@@ -39,28 +39,15 @@ const StyledWork = styled.div`
       margin-left: 20px;
     }
   }
+  
+  @media ${portfolioTheme.media.desktop} {
+    max-width: 540px;
+  }
 `
 
 const ImageWrapper = styled.div`
   position: relative;
-
-  &:hover {
-    &::before {
-      content: "";
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      background: rgba(0, 0, 0, 0.30);
-      backdrop-filter: blur(4px);
-    }
-
-    ${Button} {
-      opacity: 1;
-    }
-  }
-
+  
   ${Button} {
     opacity: 0;
     position: absolute;
@@ -71,6 +58,38 @@ const ImageWrapper = styled.div`
     &::before {
       width: 100%;
       height: 100%;
+    }
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.30);
+    backdrop-filter: blur(4px);
+    opacity: 0;
+  }
+  
+  &:hover {
+    &::before {
+      opacity: 1;
+    }
+
+    ${Button} {
+      opacity: 1;
+    }
+  }
+
+  @media ${portfolioTheme.media.tablet} {
+    &::before {
+      opacity: 1;
+    }
+
+    ${Button} {
+      opacity: 1;
     }
   }
 `
