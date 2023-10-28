8bb7fd435545f14ff04e1c348ab915a2e6cd38d3
dnisaev <dnisaev@yandex.ru>
2023-10-28 22:55:07 +0300

    refact Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,84 +1,33 @@
 import React from 'react';
-import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
-import {portfolioTheme} from "../../styles/PortfolioTheme";
-import {font} from "../../styles/Common";
+import { S } from './Footer_Styles';
 
-export const Footer = () => {
+const socialItemsData = [
+    {iconId: "instagram"},
+    {iconId: "telegram"},
+    {iconId: "vk"},
+    {iconId: "linkedin"}
+]
+
+export const Footer: React.FC = () => {
     return (
-        <StyledFooter>
+        <S.Footer>
             <FlexWrapper direction={"column"} align={"center"}>
-                <Name>Dmitriy</Name>
-                <SocialList>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"instagram"}/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"telegram"}/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"vk"}/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={"linkedin"}/>
-                        </SocialLink>
-                    </SocialItem>
-                </SocialList>
-                <Copyright>© 2023 Dmitriy Isaev, All Rights Reserved.</Copyright>
+                <S.Name>Dmitriy</S.Name>
+                <S.SocialList>
+                    {socialItemsData.map((s, index) => {
+                        return (
+                            <S.SocialItem key={index}>
+                                <S.SocialLink>
+                                    <Icon width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"} iconId={s.iconId}/>
+                                </S.SocialLink>
+                            </S.SocialItem>
+                        )
+                    })}
+                </S.SocialList>
+                <S.Copyright>© 2023 Dmitriy Isaev, All Rights Reserved.</S.Copyright>
             </FlexWrapper>
-        </StyledFooter>
+        </S.Footer>
     );
 };
-
-const StyledFooter = styled.footer`
-  background-color: ${portfolioTheme.colors.primaryBg};
-  padding: 40px 0;
-`
-
-const Name = styled.span`
-  ${font({family: "'Josefin Sans', sans-serif", weight: 700, fMax: 22, fMin: 16})}
-  letter-spacing: 3px;
-`
-
-const SocialList = styled.ul`
-  display: flex;
-  gap: 20px;
-  margin: 30px 0;
-`
-
-const SocialItem = styled.li`
-
-`
-
-const SocialLink = styled.a`
-  background-color: rgba(255, 255, 255, 0.1);
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  color: ${portfolioTheme.colors.accent};
-  
-  &:hover {
-    color: ${portfolioTheme.colors.primaryBg};
-    transform: translateY(-4px);
-    background-color: ${portfolioTheme.colors.accent};
-  }
-`
-
-const Copyright = styled.small`
-  font-weight: 400;
-  font-size: 12px;
-  text-align: center;
-  opacity: 0.5;
-`
---
+++ src/layout/footer/Footer_Styles.ts
@@ -0,0 +1,56 @@
+import {portfolioTheme} from "../../styles/PortfolioTheme";
+import styled from "styled-components";
+import {font} from "../../styles/Common";
+
+
+const Footer = styled.footer`
+  background-color: ${portfolioTheme.colors.primaryBg};
+  padding: 40px 0;
+`
+
+const Name = styled.span`
+  ${font({family: "'Josefin Sans', sans-serif", weight: 700, fMax: 22, fMin: 16})}
+  letter-spacing: 3px;
+`
+
+const SocialList = styled.ul`
+  display: flex;
+  gap: 20px;
+  margin: 30px 0;
+`
+
+const SocialItem = styled.li``
+
+const SocialLink = styled.a`
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: ${portfolioTheme.colors.accent};
+
+  &:hover {
+    color: ${portfolioTheme.colors.primaryBg};
+    transform: translateY(-4px);
+    background-color: ${portfolioTheme.colors.accent};
+  }
+`
+
+const Copyright = styled.small`
+  font-weight: 400;
+  font-size: 12px;
+  text-align: center;
+  opacity: 0.5;
+`
+
+export const S = {
+    Footer,
+    Name,
+    SocialList,
+    SocialItem,
+    SocialLink,
+    Copyright
+}
