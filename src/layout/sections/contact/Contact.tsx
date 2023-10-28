1013ad07337b46f44c1162d1cda57a06605c6b77
dnisaev <dnisaev@yandex.ru>
2023-10-28 22:44:37 +0300

    refact Contact

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,62 +1,21 @@
 import React from 'react';
-import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
-import {portfolioTheme} from "../../../styles/PortfolioTheme";
+import { S } from './Contact_Styles';
 
-export const Contact = () => {
+export const Contact: React.FC = () => {
     return (
-        <StyledContact>
+        <S.Contact>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <StyledForm>
-                    <Field placeholder={"name"}/>
-                    <Field placeholder={"email"}/>
-                    <Field placeholder={"message"} as={"textarea"}/>
+                <S.Form>
+                    <S.Field placeholder={"name"}/>
+                    <S.Field placeholder={"email"}/>
+                    <S.Field placeholder={"message"} as={"textarea"}/>
                     <Button type={"submit"}>Send message</Button>
-                </StyledForm>
+                </S.Form>
             </Container>
-        </StyledContact>
+        </S.Contact>
     );
 };
-
-const StyledContact = styled.section``
-
-const StyledForm = styled.form`
-  max-width: 540px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  margin: 0 auto;
-  align-items: center;
-  
-  textarea {
-    resize: none;
-    height: 155px;
-  }
-`
-
-const Field = styled.input`
-  width: 100%;
-  background-color: ${portfolioTheme.colors.secondaryBg};
-  border: 1px solid ${portfolioTheme.colors.border};
-  padding: 7px 15px;
-
-  font-family: Poppins, sans-serif;
-  font-size: 12px;
-  font-weight: 400;
-  letter-spacing: 0.6px;
-  
-  color: ${portfolioTheme.colors.font};
-  
-  &::placeholder {
-    color: ${portfolioTheme.colors.placeholder};
-    text-transform: capitalize;
-  }
-  
-  &:focus-visible {
-    outline: 1px solid ${portfolioTheme.colors.placeholder};
-  }
-`
---
+++ src/layout/sections/contact/Contact_Styles.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import {portfolioTheme} from "../../../styles/PortfolioTheme";
+
+
+const Contact = styled.section``
+
+const Form = styled.form`
+  max-width: 540px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin: 0 auto;
+  align-items: center;
+  
+  textarea {
+    resize: none;
+    height: 155px;
+  }
+`
+
+const Field = styled.input`
+  width: 100%;
+  background-color: ${portfolioTheme.colors.secondaryBg};
+  border: 1px solid ${portfolioTheme.colors.border};
+  padding: 7px 15px;
+
+  font-family: Poppins, sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.6px;
+  
+  color: ${portfolioTheme.colors.font};
+  
+  &::placeholder {
+    color: ${portfolioTheme.colors.placeholder};
+    text-transform: capitalize;
+  }
+  
+  &:focus-visible {
+    outline: 1px solid ${portfolioTheme.colors.placeholder};
+  }
+`
+
+export const S = {
+    Contact,
+    Form,
+    Field
+}
