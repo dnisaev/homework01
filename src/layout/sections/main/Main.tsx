b74c3d81d0d96150cc99e2d1424b489049bcd7ad
dnisaev <dnisaev@yandex.ru>
2023-11-01 00:52:13 +0300

    add useState(greetings) to Main.tsx

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import photo from '../../../assets/images/photo.jpg';
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import { S } from './Main_Styles';
 
 export const Main: React.FC = () => {
+
+    const greetings = {
+        first: "Hi There",
+        second: "Hello my dear friend"
+    };
+
+    const [greeting, setGreeting] = useState(greetings.first);
+
+    const changeGreeting = () => setGreeting(greetings.second);
+    const changeGreetingBack = () => setGreeting(greetings.first);
     return (
         <S.Main>
             <Container>
                 <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
                     <S.MainText>
-                        <S.Greeting>Hi There</S.Greeting>
+                        <S.Greeting>{greeting}</S.Greeting>
                         <S.Name>I am <span>Dmitriy Isaev</span></S.Name>
                         <S.MainTitle>A Front-end Developer.</S.MainTitle>
                     </S.MainText>
                     <S.PhotoWrapper>
-                        <S.Photo src={photo} alt={""}/>
+                        <S.Photo onClick={changeGreeting} onDoubleClick={changeGreetingBack} src={photo} alt={""}/>
                     </S.PhotoWrapper>
                 </FlexWrapper>
             </Container>
